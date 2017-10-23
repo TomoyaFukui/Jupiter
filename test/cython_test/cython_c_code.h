75e6d848f437cc4968e2d8a0b422fa10388d95ad
Fukui Tomoya <sumabura6581@gmail.com>
2017-10-23 12:37:01 +1100

    add -u

--- test/cython_test/cython_c_code.h
+++
@@ -1,4 +0,0 @@
-#ifndef CYTHON_CODE
-#define CYTHON_CODE
-int c_algo(int*, int*, int, int);
-#endif
--- test/jcc_test/jcc_test.py
+++
@@ -1,4 +0,0 @@
-import pyhellojcc
-pyhellojcc.initVM()
-shello = pyhellojcc.HelloJCC("ooo")
-#shello.sayHello()
