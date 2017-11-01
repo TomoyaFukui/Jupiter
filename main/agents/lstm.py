09deec5d8a59690d3c30b0d0fc4af20cd444bf10
Fukui Tomoya <sumabura6581@gmail.com>
2017-11-01 12:46:23 +1100

    save lstm.py

--- main/agents/lstm.py
+++ main/agents/lstm.py
@@ -1,54 +1,32 @@
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+import json
 
-np.random.seed(0)
-tf.set_random_seed(1234)
-
-
-def inference(x, n_batch, maxlen=None, n_hidden=None, n_out=None):
-    def weight_variable(shape):
-        initial = tf.truncated_normal(shape, stddev=0.01)
-        return tf.Variable(initial)
-
-    def bias_variable(shape):
-        initial = tf.zeros(shape, dtype=tf.float32)
-        return tf.Variable(initial)
-
-    cell = tf.contrib.rnn.LSTMCell(n_hidden, forget_bias=1.0)
-    initial_state = cell.zero_state(n_batch, tf.float32)
-
-    state = initial_state
-    outputs = []  # 過去の隠れ層の出力を保存
-    with tf.variable_scope('LSTM'):
-        for t in range(maxlen):
-            if t > 0:
-                tf.get_variable_scope().reuse_variables()
-            (cell_output, state) = cell(x[:, t, :], state)
-            outputs.append(cell_output)
-
-    output = outputs[-1]
-
-    V = weight_variable([n_hidden, n_out])
-    c = bias_variable([n_out])
-    y = tf.matmul(output, V) + c  # 線形活性
-
-    return y
 
 
 def loss(y, t):
-    mse = tf.reduce_mean(tf.square(y - t))
-    return mse
+    cross_entropy = \
+        tf.reduce_mean(-tf.reduce_sum(
+                       t * tf.log(tf.clip_by_value(y, 1e-10, 1.0)),
+                       reduction_indices=[1]))
+    return cross_entropy
 
 
 def training(loss):
-    optimizer = \
-        tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.9, beta2=0.999)
-
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.1,
+                                       beta1=0.9,
+                                       beta2=0.999)
     train_step = optimizer.minimize(loss)
     return train_step
+
+def accuracy(y, t):
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(t, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    return accuracy
 
 
 class EarlyStopping():
@@ -73,65 +51,60 @@ class EarlyStopping():
 
 
 if __name__ == '__main__':
-    def sin(x, T=100):
-        return np.sin(2.0 * np.pi * x / T)
-
-    def toy_problem(T=100, ampl=0.05):
-        x = np.arange(0, 2 * T + 1)
-        noise = ampl * np.random.uniform(low=-1.0, high=1.0, size=len(x))
-        return sin(x) + noise
-
     '''
     データの生成
     '''
-    T = 100
-    f = toy_problem(T)
+    f = open("bids.json", 'r')
+    json_data = json.load(f) #JSON形式で読み込む
+    X = json_data["in"]
+    Y = json_data["out"]
 
-    length_of_sequences = 2 * T
-    maxlen = 25
+    N_train = int(len(X) * 0.9)
+    N_validation = len(X) - N_train
 
-    data = []
-    target = []
-
-    for i in range(0, length_of_sequences - maxlen + 1):
-        data.append(f[i: i + maxlen])
-        target.append(f[i + maxlen])
-
-    X = np.array(data).reshape(len(data), maxlen, 1)
-    Y = np.array(target).reshape(len(data), 1)
-
-    # データ設定
-    N_train = int(len(data) * 0.9)
-    N_validation = len(data) - N_train
+    X_train, X_test, Y_train, Y_test = \
+        train_test_split(X, Y, train_size=N_train)
 
     X_train, X_validation, Y_train, Y_validation = \
-        train_test_split(X, Y, test_size=N_validation)
+        train_test_split(X_train, Y_train, test_size=N_validation)
+
+    #print(X_train[0])
 
     '''
     モデル設定
     '''
-    n_in = len(X[0][0])  # 1
-    n_hidden = 30
-    n_out = len(Y[0])  # 1
+    #n_in = len(X[0])
+    #n_in = [3, 3]
+    #n_hiddens = [3, 1]  # 各隠れ層の次元数
+    #n_out = len(Y[0])
+    #n_out = 3
 
-    x = tf.placeholder(tf.float32, shape=[None, maxlen, n_in])
-    t = tf.placeholder(tf.float32, shape=[None, n_out])
-    n_batch = tf.placeholder(tf.int32, shape=[])
+    np.random.seed(0)
+    tf.set_random_seed(1234)
 
-    y = inference(x, n_batch, maxlen=maxlen, n_hidden=n_hidden, n_out=n_out)
+    x = tf.placeholder(tf.float32, shape=[None, 3])
+    t = tf.placeholder(tf.float32, shape=[None, 2])
+
+    #y = inference(x, n_in=n_in, n_hiddens=n_hiddens, n_out=n_out)
+    w = tf.Variable(tf.zeros([3, 2]))
+    b = tf.Variable(tf.zeros([2]))
+    y = tf.nn.sigmoid(tf.matmul(x, w) + b)
     loss = loss(y, t)
     train_step = training(loss)
 
+    accuracy = accuracy(y, t)
     early_stopping = EarlyStopping(patience=10, verbose=1)
+
     history = {
-        'val_loss': []
+        'val_loss': [],
+        'val_acc': []
     }
 
     '''
     モデル学習
     '''
-    epochs = 500
-    batch_size = 10
+    epochs = 10
+    batch_size = 1
 
     init = tf.global_variables_initializer()
     sess = tf.Session()
@@ -142,61 +115,55 @@ if __name__ == '__main__':
     for epoch in range(epochs):
         X_, Y_ = shuffle(X_train, Y_train)
 
-        for i in range(n_batches):
-            start = i * batch_size
-            end = start + batch_size
-
-            sess.run(train_step, feed_dict={
-                x: X_[start:end],
-                t: Y_[start:end],
-                n_batch: batch_size
-            })
+        #for i in range(n_batches):
+        #    start = i * batch_size
+        #    end = start + batch_size
+        #    sess.run(train_step, feed_dict={
+        #        x: X_[start:end],
+        #        t: Y_[start:end]
+        #    })
+        sess.run(train_step, feed_dict={
+            x: X_,
+            t: Y_
+        })
 
         # 検証データを用いた評価
         val_loss = loss.eval(session=sess, feed_dict={
             x: X_validation,
-            t: Y_validation,
-            n_batch: N_validation
+            t: Y_validation
+        })
+        val_acc = accuracy.eval(session=sess, feed_dict={
+            x: X_validation,
+            t: Y_validation
         })
 
+        # 検証データに対する学習の進み具合を記録
         history['val_loss'].append(val_loss)
+        history['val_acc'].append(val_acc)
+
         print('epoch:', epoch,
-              ' validation loss:', val_loss)
+              ' validation loss:', val_loss,
+              ' validation accuracy:', val_acc)
 
         # Early Stopping チェック
         if early_stopping.validate(val_loss):
             break
 
     '''
-    出力を用いて予測
-    '''
-    truncate = maxlen
-    Z = X[:1]  # 元データの最初の一部だけ切り出し
-
-    original = [f[i] for i in range(maxlen)]
-    predicted = [None for i in range(maxlen)]
-
-    for i in range(length_of_sequences - maxlen + 1):
-        # 最後の時系列データから未来を予測
-        z_ = Z[-1:]
-        y_ = y.eval(session=sess, feed_dict={
-            x: Z[-1:],
-            n_batch: 1
-        })
-        # 予測結果を用いて新しい時系列データを生成
-        sequence_ = np.concatenate(
-            (z_.reshape(maxlen, n_in)[1:], y_), axis=0) \
-            .reshape(1, maxlen, n_in)
-        Z = np.append(Z, sequence_, axis=0)
-        predicted.append(y_.reshape(-1))
-
-    '''
-    グラフで可視化
+    学習の進み具合を可視化
     '''
     plt.rc('font', family='serif')
-    plt.figure()
-    plt.ylim([-1.5, 1.5])
-    plt.plot(toy_problem(T, ampl=0), linestyle='dotted', color='#aaaaaa')
-    plt.plot(original, linestyle='dashed', color='black')
-    plt.plot(predicted, color='black')
+    fig = plt.figure()
+    plt.plot(range(len(history['val_loss'])), history['val_loss'],
+             label='loss', color='black')
+    plt.xlabel('epochs')
     plt.show()
+
+    '''
+    予測精度の評価
+    '''
+    accuracy_rate = accuracy.eval(session=sess, feed_dict={
+        x: X_test,
+        t: Y_test
+    })
+    print('accuracy: ', accuracy_rate)
